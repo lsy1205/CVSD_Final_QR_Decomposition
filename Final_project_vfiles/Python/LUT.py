2de0bbb94afd25e9454c86948b82a8122816a341
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2023-12-12 21:54:29 +0800

    add LUT python

--- Final_project_vfiles/Python/LUT.py
+++ Final_project_vfiles/Python/LUT.py
@@ -1,8 +1,8 @@
 LUT = []
 outfile = open("LUT.txt", "w")
 
-for i in range(32):
-    # print(round(1/(64+i)*(2**21)))
+for i in range(64):
+    print(round(1/(64+i)*(2**21)))
     # print(bin(round(1/(64+i)*(2**21)))[2:].zfill(16))
     if i == 0:
         LUT.append("0111111111111111")
@@ -10,7 +10,7 @@ for i in range(32):
         LUT.append(bin(round(1/(64+i)*(2**21)))[2:].zfill(16))
 
 for i in range(len(LUT)):
-    print(bin(i)[2:].zfill(5))
-    outfile.write("5'b"+bin(i)[2:].zfill(5) + ": begin\n")
+    # print(bin(i)[2:].zfill(6))
+    outfile.write("6'b"+bin(i)[2:].zfill(6) + ": begin\n")
     outfile.write("    reciprocal = 16'b" + LUT[i] + ";\n")
     outfile.write("end\n")
