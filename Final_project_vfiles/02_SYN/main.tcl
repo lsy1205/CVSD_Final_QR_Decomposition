97ad0a54c36a155aebac706562c53d9d76305e07
lsy1205 <75497265+lsy1205@users.noreply.github.com>
2023-12-15 17:04:13 +0800

    give up clock gating

--- Final_project_vfiles/02_SYN/main.tcl
+++ Final_project_vfiles/02_SYN/main.tcl
@@ -7,7 +7,7 @@ set compile_fix_multiple_port_nets "TRUE"
 
 set DESIGN "QR_Engine"
 set CLOCK "i_clk"
-set CLOCK_PERIOD 4.5
+set CLOCK_PERIOD 5.0
 
 sh rm -rf Netlist
 sh rm -rf Report
@@ -33,29 +33,17 @@ set_max_fanout 8 [current_design]
 set_operating_conditions -max_library slow -max slow
 set_wire_load_model -name tsmc13_wl10 -library slow
 # =================================================== #
-check_design > Report/check_design.txt
-check_timing > Report/check_timing.txt
-
-# Clock Gating
-set_clock_gating_style \
-    -max_fanout 8 \
-    -pos integrated \
-    -control_point before \
-    -control_signal scan_enable
-
+check_design
 uniquify
 set_fix_multiple_port_nets -all -buffer_constants  [get_designs *]
 set_fix_hold [all_clocks]
 
-compile_ultra -gate_clock
+compile_ultra
 
 report_area > Report/$DESIGN\.area
 report_power > Report/$DESIGN\.power
 report_timing -max_path 20 -delay_type max > Report/$DESIGN\.max.timing
 report_timing -max_path 20 -delay_type min > Report/$DESIGN\.min.timing
-
-report_clock_gating -gating_elements > "./Report/${DESIGN}_syn.gating_elements"
-report_clock_gating -ungated > "./Report/${DESIGN}_syn.ungated"
 
 set bus_inference_style "%s\[%d\]"
 set bus_naming_style "%s\[%d\]"
